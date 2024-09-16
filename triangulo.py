cce798cef97fb9f21a887f8333e4bf5ff63f76fc
BeatrizVezzali <beatrizandradevezzali@gmail.com>
2024-09-16 18:51:54 -0300

    Mini projeto Python

--- triangulo.py
+++ triangulo.py
@@ -2,16 +2,13 @@ A = int(input("Digite o primeiro número:"))
 B = int(input("Digite o segundo número:"))
 C = int(input("Digite o terceiro número:"))
 
-A < B + C
-B < A + C
-C < A + B
+if (A < B+C) and (B < A+C) and (C < A+B):
+    if (A == B) and (B == C):
+        print("É um triângulo equilátero",A,B,C)
+    elif (A == B) or (B == C) or (C == A):
+          print("É um triângulo isóceles",A,B,C)
+    else:
+        print("É um triângulo escaleno",A,B,C)
+else:
+    print("Não é um triângulo",A,B,C)
 
-if (A == B) or (A == C) or (B == C) :
-    print("Triângulo isósceles")
-else :
-    if (A!= B) and (B!= C) :
-        print("Triângulo escaleno")
-    else :
-       print("Triângulo equilátero")
-
-   

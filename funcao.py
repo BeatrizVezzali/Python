ac2b66eb89c489f1321f0b1d4af6361f05052b6d
BeatrizVezzali <beatrizandradevezzali@gmail.com>
2024-11-05 17:08:10 -0300

    Mini projeto Python

---
+++ funcao.py
@@ -0,0 +1,11 @@
+def soma(x, y, z) :
+       return x + y + z
+
+def mult(x, y, z) :
+      return x * y * z
+
+def exp(x, y) :
+      print(x ** y)
+
+def meunome() :
+      return "Jacskoprodenílson Astorbaldo"

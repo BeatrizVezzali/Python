ac1001049ff308c28514e38912e5711e49b32cb6
BeatrizVezzali <beatrizandradevezzali@gmail.com>
2024-11-08 19:00:26 -0300

    Mini projeto Python

---
+++ lista_frutas.py
@@ -0,0 +1,12 @@
+frutas = ["Pera", "Morango", "Abacaxi", "Melancia"]
+x = frutas.count("Abacaxi")
+print(x)
+
+frutas2 = ["Uva", "Limão", "Mexerica", "Abacate"]
+sorted(frutas2)
+frutas2.sort()
+print(frutas2)
+frutas2.reverse()
+print(frutas2)
+frutas2.pop(3)
+print(frutas2)

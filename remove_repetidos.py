45b70e0585800824e8e308e68f80c928c767404a
BeatrizVezzali <beatrizandradevezzali@gmail.com>
2024-11-13 13:47:34 -0300

    Mini projeto Python

--- remove_repetidos.py
+++ remove_repetidos.py
@@ -2,8 +2,8 @@ lista = [10,10,4,6,7,8,9,9,11,1,6,5,3]
 
 def remove_repetidos(lista):
     lista1 = []
-    for elemento in lista:
-        if (elemento not in lista1):
+    for i in lista:
+        if (i not in lista1):
             lista1.append(elemento)
     lista1.sort()
     return lista1
